e0912c3222c601e04ab037fd8040a60aa69252cc
Evandro Minamoto <89124140+Otomanim@users.noreply.github.com>
2024-09-05 20:36:49 -0300

    Update

--- QuizGame/Scenes/Score/FinalScoreViewController.swift
+++ QuizGame/Scenes/Score/FinalScoreViewController.swift
@@ -20,4 +20,22 @@ class FinalScoreViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        
+    }
+    
+    @objc private func retry() {
+        coordinator.start()
+    }
+    
+    @objc private func exitQuiz() {
+        coordinator.backToStart()
+    }
 }
